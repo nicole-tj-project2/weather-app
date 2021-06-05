0aac160aed30eb0dde422b94640c04a17af10129
NicoleBrady <nicolebrady.dev@gmail.com>
2021-06-05 17:52:24 -0400

    get location needs debugging

--- script.js
+++ script.js
@@ -1,75 +1,27 @@
-
-// fetch('http://api.weatherstack.com/current?access_key=f1a05eb255c245dce0247a76569643ad&query=Toronto')
-// .then(function(response){
-//      return response.json();
-//     })
-//  .then(function(weatherData){
-//    console.log(weatherData);
-//  })
-    
-    
-    // User selects location from predetermined options in a dropdown
-    // when seleted, submit
-    // bbfcaf59532e863784c25652f4481a55
-
-
-    // Based on what is selected, output data
-
-
-//     fetch('http://api.weatherstack.com/current?access_key=f1a05eb255c245dce0247a76569643ad&query=Toronto')
-// .then(function(response){
-//      return response.json();
-//     })
-//  .then(function(weatherData){
-//    console.log(weatherData);
-//  })
-
-// const url = new URL('http://api.weatherstack.com/current');
-
-// url.search = new URLSearchParams({
-//   access_key: 'f1a05eb255c245dce0247a76569643ad',
-//   query: 'Toronto'
-// });
-
-// fetch(url)
-// .then(function(response){
-//   return response.json();
-//  })
-// .then(function(weatherData){
-// console.log(weatherData);
-// })
-
-// bbfcaf59532e863784c25652f4481a55
-// fetch('https://api.openweathermap.org/data/2.5/weather?q=toronto&appid=bbfcaf59532e863784c25652f4481a55')
-// .then(function(response){
-//   return response.json();
-// })
-// .then(function(weatherData){
-//   console.log(weatherData);
-// })
-
-
-// tcAKLyC3UWNqsRBFm1EqKLYvDJTCxajG 
-// fetch('http://dataservice.accuweather.com/currentconditions/v1/toronto"details=true"')
-// .then(function(response){
-//   return response.json();
-// })
-// .then(function(weatherData){
-//   console.log(weatherData);
-// })
-
 const weatherApp = {};
 
 weatherApp.geoURL = 'http://api.openweathermap.org/geo/1.0/direct';
 weatherApp.oneCallURL = 'https://api.openweathermap.org/data/2.5/onecall';
 weatherApp.key = 'bbfcaf59532e863784c25652f4481a55';
 
+// Retreiving input from user
+weatherApp.getLocation = () => {
+// retrieve the form element
+    const formEl = document.querySelector("select");
+    formEl.addEventListener("change", (e) => {
+        e.preventDefault();
+        // const selEl = document.getElementById("location");
+        inputValue = formEl.value;
+    })
+    return inputValue;
+}
+
 // getting the coordinates with GEOCODING API
-weatherApp.getCoordinates = () => {
+weatherApp.getCoordinates = (location) => {
     // parameters
     const geoApiUrl = new URL(weatherApp.geoURL)
     geoApiUrl.search = new URLSearchParams({
-        q: 'Toronto',
+        q: location,
         appid: weatherApp.key
     })
     fetch(geoApiUrl)
@@ -77,8 +29,24 @@ weatherApp.getCoordinates = () => {
         return results.json()
     })
     .then(function(geoData){
+        console.log(geoData)
         const lat = geoData[0].lat;
-        console.log(lat);
+        const lon = geoData[0].lon;
+        console.log(`${lat}${lon}`)
+        const oneCallURL = new URL(weatherApp.oneCallURL)
+        oneCallURL.search = new URLSearchParams({
+            lat: lat,
+            lon: lon,
+            appid: weatherApp.key
+        })
+        fetch(oneCallURL)
+            .then(function(data){
+                // console.log(weatherData)
+                return data.json()
+            })
+            .then(function(weatherData){
+                console.log(weatherData);
+            })
     })
 }
 
@@ -86,7 +54,8 @@ weatherApp.getCoordinates = () => {
 
 // Init
 weatherApp.init = () => {
-    weatherApp.getCoordinates()
+    const location = weatherApp.getLocation();
+    weatherApp.getCoordinates(location);
 }
 
 weatherApp.init();
